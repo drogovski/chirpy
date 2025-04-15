d62d3f87e0c83bd957f3f4d553ef68053a8e3418
Daniel Rogowski <drogovski@proton.me>
2025-04-15 19:03:28 +0200

    add updating the user credentials

--- main.go
+++ main.go
@@ -59,6 +59,7 @@ func main() {
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
 
 	mux.HandleFunc("POST /api/users", apiCfg.handlerUsersCreate)
+	mux.HandleFunc("PUT /api/users", apiCfg.handlerChangeCredentials)
 	mux.HandleFunc("POST /api/login", apiCfg.handlerLogin)
 	mux.HandleFunc("POST /api/refresh", apiCfg.handlerRefreshToken)
 	mux.HandleFunc("POST /api/revoke", apiCfg.handlerRevokeToken)
